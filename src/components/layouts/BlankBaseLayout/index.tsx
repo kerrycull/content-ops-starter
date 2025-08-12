54d5feecb0f10a39dd042999f8f90dce541ea641
kerrycull <kerrywcull@hotmail.com>
2025-08-12 20:57:14 +0100

    Update index.tsx

--- src/components/layouts/BlankBaseLayout/index.tsx
+++ src/components/layouts/BlankBaseLayout/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import classNames from 'classnames';
 
 export default function BlankBaseLayout(props) {
     const { page, site } = props;
     const { enableAnnotations = true } = site;
     const pageMeta = page?.__metadata || {};
+
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
             <Head>
@@ -13,14 +15,18 @@ export default function BlankBaseLayout(props) {
                 <meta name="description" content="Components Library" />
                 {site.favicon && <link rel="icon" href={site.favicon} />}
             </Head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17462240755"></script>
-            <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', 'AW-17462240755');
-            </script>
+            {/* Google tag (gtag.js) */}
+            <Script src="https://www.googletagmanager.com/gtag/js?id=AW-17462240755" strategy="afterInteractive" />
+            <Script id="gtag-init" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17462240755');
+        `}
+            </Script>
+
             {props.children}
         </div>
     );
