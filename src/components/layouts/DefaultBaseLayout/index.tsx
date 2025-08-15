d589e5186e7271cdcd99ebfa2126f8ae7f783eff
kerrycull <kerrywcull@hotmail.com>
2025-08-15 14:51:52 +0100

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Head from 'next/head';
 import classNames from 'classnames';
 import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
@@ -10,6 +11,27 @@ export default function DefaultBaseLayout(props) {
 
     return (
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
+            <Head>
+                {/* Page meta */}
+                <title>{page?.title}</title>
+                <meta name="description" content="Components Library" />
+                {site?.favicon && <link rel="icon" href={site.favicon} />}
+
+                {/* Google tag (gtag.js) */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17471990262"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.gtag = window.gtag || gtag;
+              gtag('js', new Date());
+              gtag('config', 'AW-17471990262');
+            `
+                    }}
+                />
+            </Head>
+
             <div className="sb-base sb-default-base-layout">
                 {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
                 {props.children}
